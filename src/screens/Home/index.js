1eac7c9c9810bc357855ac3d88409502cbc4d4e8
SAKET KUMAR <106534885+saketkumar9040@users.noreply.github.com>
2023-09-20 15:33:09 +0530

    style - started with styling home screen

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,11 +1,18 @@
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity } from 'react-native'
 import React from 'react'
+import { Entypo } from '@expo/vector-icons';
+import { styles } from './style';
 
 const HomeScreen = () => {
   return (
-    <View>
+    <>
+    <View style={styles.mainContainer}>
       <Text>HomeScreen</Text>
     </View>
+     <TouchableOpacity style={styles.addTaskContainer}>
+     <Entypo name="add-to-list" size={40} color="#fff" />
+     </TouchableOpacity>
+    </>
   );
 }
 
---
+++ src/screens/Home/style.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    mainContainer:{
+        flex:1
+    },
+
+    addTaskContainer:{
+        marginVertical:10,
+        alignSelf:"center",
+        backgroundColor:"#81d4fa",
+        padding:15,
+        borderRadius:50
+    }
+})
